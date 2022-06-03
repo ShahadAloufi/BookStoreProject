4260780bbe46ba42892d4c224d585818d1d9e66b
shahdmefleh <69356275+shahdmefleh@users.noreply.github.com>
2022-06-03 16:16:23 +0300

    Initial Commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by shahadmufleh on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
